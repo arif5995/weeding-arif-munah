7eed65d4b98396efee91e06604a3e052bf12751e
arif5995 <52032920+arif5995@users.noreply.github.com>
2026-08-17 20:30:19 +0700

    bug animation

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,3 +1,20 @@
 export function cn(...inputs) {
-  return inputs.filter(Boolean).join(" ");
+  const classes = [];
+
+  for (const input of inputs) {
+    if (!input) continue;
+
+    if (typeof input === "string") {
+      classes.push(input);
+    } else if (Array.isArray(input)) {
+      classes.push(cn(...input));
+    } else if (typeof input === "object") {
+      // Dukung syntax { "nama-class": kondisiBoolean }
+      for (const key in input) {
+        if (input[key]) classes.push(key);
+      }
+    }
+  }
+
+  return classes.join(" ");
 }
