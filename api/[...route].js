61af93a49927ed4791d192e78c1880e3294e768f
arif5995 <52032920+arif5995@users.noreply.github.com>
2026-08-18 17:09:52 +0700

    change route js

--- api/[...route].js
+++ api/[...route].js
@@ -1,8 +1,4 @@
 import { handle } from "hono/vercel";
 import app from "../src/server/index.js";
 
-export const GET = handle(app);
-export const POST = handle(app);
-export const PUT = handle(app);
-export const DELETE = handle(app);
-export const PATCH = handle(app);
+export default handle(app);
